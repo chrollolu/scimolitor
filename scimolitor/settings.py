06444e55fabef787669917b04b3a4d4dfb72ea18
chrollolu <barrythierno30@gmail.com>
2023-03-30 13:10:22 +0100

    lot de correction 1

--- scimolitor/settings.py
+++ scimolitor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4$bswl!u1iqx@2nq&i*+6=qy1y9*y786(7t$34thkxklpf^&l#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
